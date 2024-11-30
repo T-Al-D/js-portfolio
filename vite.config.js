c27241781d9ee58e42d1fc0dd0ca55653dd3866f
T-Al-D <tahaaldailami2010@hotmail.de>
2024-11-30 15:41:25 +0100

    changed vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: "https://t-al-d.github.io/js-portfolio/",
+	base: "/t-al-d.github.io/js-portfolio/",
 });
