d38038f4f32789f419a32a69e25ee1c04ac91de3
T-Al-D <tahaaldailami2010@hotmail.de>
2024-12-15 17:12:36 +0100

    added code to nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,35 +3,47 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 // hash-based routing
-
 let navArray = [
 	{ id: "Home", link: "/" },
 	{ id: "About", link: "/about" },
 	{ id: "Projects", link: "/projects" },
 ];
+export function DropDown({ setMenuOpen }) {
+	return (
+		<div className="dropdown">
+			<ul className="dropdown-list">
+				{navArray.map((item) => (
+					<Link
+						className="nav-link"
+						key={item.id}
+						to={item.link}
+						onClick={() => setMenuOpen(false)}
+					>
+						{item.id}
+					</Link>
+				))}
+			</ul>
+		</div>
+	);
+}
 
 export default function Nav() {
 	const [menuOpen, setMenuOpen] = useState(false);
 
-	// Toggle the mobile menu
-	const toggleMenu = () => {
-		setMenuOpen(!menuOpen);
-	};
-
 	return (
 		<div className="nav-wrapper">
 			{/* Button to toggle menu on small screens */}
-			<button className="menu-toggle" onClick={toggleMenu}>
+			<button
+				className="menu-toggle"
+				onClick={() => setMenuOpen(!menuOpen)}
+			>
 				MENU
 			</button>
 			{/* Small navigation (visible on mobile) */}
 
-			<nav className={`nav-small ${menuOpen ? "open" : ""}`}>
-				{navArray.map((item) => (
-					<Link key={item.id} to={item.link} className="nav-link">
-						{item.id}
-					</Link>
-				))}
+			<nav className="nav-small">
+				{/* only open navigation if button toggled */}
+				{menuOpen && <DropDown setMenuOpen={setMenuOpen} />}
 			</nav>
 
 			<nav className="nav-large">
