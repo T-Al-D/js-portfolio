8fb3c30800aa5fc088ef04e945a82e6a99c4e257
T-Al-D <tahaaldailami2010@hotmail.de>
2025-01-02 11:10:38 +0100

    started animating Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ export default function Home() {
 	return (
 		<div>
 			<pre className="basic-text"> Hello, I´m T-Al-D ! </pre>
-			<div className="programming-background">
+			<div className="animated-background">
 				<img src={programmingBackground} alt=""></img>
 			</div>
 		</div>
